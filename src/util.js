5f87895f56bf02d44e6b9508cab1fe46d7bb6407
Alexander Stensrud <alex@stensrud.nu>
2016-02-15 11:51:18 +0100

    isComponent

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
-import { compose, is, path } from 'ramda'
+import { compose, is, path, prop } from 'ramda'
 
-export const isClassComponent = compose(
-	is(Object), path(['prototype', 'isReactComponent'])
-)
+export const isClassComponent = compose(is(Object), path(['prototype', 'isReactComponent']))
+
+export const isComponent = compose(is(Function), prop('type'))
 
